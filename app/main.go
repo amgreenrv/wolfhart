40140808b1534dd3742de9bfc72b919b915d4722
Amanda Green <amgreen@redventures.com>
2022-03-24 10:43:35 -0700

    added notes in main.go file for clarification

--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Struct for inventory items
+//Struct for inventory items, provides structure for the fetched data
 type Item struct {
 	ID       string  `json:"id"`
 	Name     string  `json:"prodname"`
@@ -24,53 +24,62 @@ type Item struct {
 }
 
 //Fetches FEATURED products from database
+//[]*Item is a slice of pointers to the struct Item, this allows me to use &item reference below in the append.
+//Also known as dereferencing or indirecting
 func getFeatured() []*Item {
-
+	//Used to create secure variables for database
 	var password = os.Getenv("DB_PASSWORD")
 	var database = os.Getenv("DB_NAME")
-	// Open up our database connection.
+
+	//Open the database connection using the secure variables - %s is basic string printing
+	//username, password, port, database name
 	db, err := sql.Open("mysql", fmt.Sprintf("root:%s@tcp(database:3306)/%s", password, database))
 	// if there is an error opening the connection, handle it
 	if err != nil {
-		log.Print(err.Error())
+		panic(err.Error()) // error handling
 	}
-	defer db.Close()
+	defer db.Close() //defer tells the database not to close until the surrounding function is complete.
 
 	// Execute the query
+	//Query only selects the data we need to serve the featured product request/route
 	results, err := db.Query("SELECT prodname, img, featured FROM products WHERE featured = 1")
+	// if there is an error opening the connection, handle it
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		panic(err.Error()) // error handling
 	}
-
+	//Scns through the query above, .Next directs it to the next item on the list then appends the items to
 	var items []*Item
 	for results.Next() {
 		var item Item
 		err = results.Scan(&item.Name, &item.Img, &item.Featured)
 		if err != nil {
-			panic(err.Error())
+			panic(err.Error()) // error handling
 		}
 		items = append(items, &item)
 	}
 
-	return items
+	return items //returning the newly appended slice of struct with data.
 }
 
 //Fetches ALL products from database
 func getInventory() []*Item {
+	//Used to create secure variables for database
 	var password = os.Getenv("DB_PASSWORD")
 	var database = os.Getenv("DB_NAME")
-	// Open up our database connection.
+
+	//Open the database connection using the secure variables
+	//username, password, port, database name
 	db, err := sql.Open("mysql", fmt.Sprintf("root:%s@tcp(database:3306)/%s", password, database))
 	// if there is an error opening the connection, handle it
 	if err != nil {
-		log.Print(err.Error())
+		panic(err.Error()) // error handling
 	}
-	defer db.Close()
+	defer db.Close() //defer tells the database not to close until the surrounding function is complete.
 
-	// Execute the query
+	// Execute the query, select everything from the products table
 	results, err := db.Query("SELECT * FROM products")
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		panic(err.Error()) // error handling
 	}
 
 	var items []*Item
@@ -78,7 +87,7 @@ func getInventory() []*Item {
 		var item Item
 		err = results.Scan(&item.ID, &item.Name, &item.Category, &item.Price, &item.Img, &item.ProdDesc, &item.Featured)
 		if err != nil {
-			panic(err.Error())
+			panic(err.Error()) // error handling
 		}
 		items = append(items, &item)
 	}
@@ -92,10 +101,12 @@ func featuredPage(w http.ResponseWriter, r *http.Request) {
 
 	items := getFeatured() //calling the getFeatured function
 
+	//returns the 200 (StatusOK) status code
 	w.WriteHeader(http.StatusOK)
+	//header establishes the type of content the browser expects to receive (json)
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Println("Endpoint Hit: featuredPage")
-	json.NewEncoder(w).Encode(items)
+	fmt.Println("Endpoint Hit: featuredPage") //console log to determine if this endpoint was hit successfully
+	json.NewEncoder(w).Encode(items)          //encodes the response in json
 
 }
 
@@ -106,23 +117,26 @@ func productPage(w http.ResponseWriter, r *http.Request) {
 	items := getInventory() //calling the getInventory function
 
 	w.WriteHeader(http.StatusOK)
+	//header establishes the type of content the browser expects to receive (json)
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Println("Endpoint Hit: productsPage")
+	fmt.Println("Endpoint Hit: productsPage") //console log to determine if this endpoint was hit successfully
 	json.NewEncoder(w).Encode(items)
 }
 
-//Main function with route handlers
+//Main function with route handlers for products and featured end points
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	//StrictSlash is a parameter on Mux router that tells it to redirect URL routes with trailing slashes /featured/ vs /featured
 
 	router.HandleFunc("/featured", featuredPage).Methods("GET")
 	router.HandleFunc("/products", productPage).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+	//logging any errors, open and listening to port 8080
 
 }
 
-//CORS error fix - Cross-Origin Resources Sharing
+//CORS error fix - Cross-Origin Resources Sharing, * = share with all
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
